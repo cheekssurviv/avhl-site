f449c51df1a569bcad81283a63a5e5007a94dae5
cheekssurviv <chriswbelcher@gmail.com>
2026-06-20 13:15:13 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen bg-white text-[#000B36]">
-      <nav className="flex justify-between items-center px-12 py-6 border-b border-[#000B36]/20">
+      <nav className="flex items-center justify-between px-5 md:px-12 py-5 border-b border-[#000B36]/20">
         <div className="flex items-center gap-3">
           <Image
             src="/avhl-logo.png"
@@ -12,15 +12,20 @@ export default function Home() {
             height={55}
           />
 
-          <h1 className="text-3xl font-black">AVHL</h1>
+          <h1 className="text-3xl md:text-4xl font-black">AVHL</h1>
         </div>
 
-        <div className="flex gap-8 font-semibold">
+        <div className="hidden md:flex gap-8 font-semibold">
           <a href="#">Home</a>
           <a href="#">Standings</a>
           <a href="#">Teams</a>
           <a href="#">Statistics</a>
           <a href="#">History</a>
+        </div>
+
+        <div className="flex md:hidden gap-5 font-semibold text-lg">
+          <a href="#">Standings</a>
+          <a href="#">Teams</a>
         </div>
       </nav>
 
@@ -30,10 +35,10 @@ export default function Home() {
           alt="AVHL Logo"
           width={320}
           height={320}
-          className="mt-2"
+          className="mt-2 w-[280px] md:w-[320px] h-auto"
         />
 
-        <h2 className="text-4xl font-bold mt-6">
+        <h2 className="text-4xl md:text-4xl font-bold mt-6 max-w-4xl">
           AMERICAN VIRTUAL HOCKEY LEAGUE
         </h2>
 
