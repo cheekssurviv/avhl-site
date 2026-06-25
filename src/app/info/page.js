cf246841482783e1e02b97e786002aabbe2d0ba3
cheekssurviv <chriswbelcher@gmail.com>
2026-06-24 20:12:45 -0400

    Info and schedule pages

---
+++ src/app/info/page.js
@@ -0,0 +1,10 @@
+export default function Info() {
+  return (
+    <main className="min-h-screen bg-white text-[#000B36] p-12">
+      <h1 className="text-5xl font-black">AVHL Info</h1>
+      <p className="mt-4 text-xl text-[#A90117]">
+        Current league info coming soon.
+      </p>
+    </main>
+  );
+}
